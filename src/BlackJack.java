bb96aa8ea1d36981c9fdb46645771b663e838ef7
Evgeny Zelenin <transingularity@gmail.com>
2016-04-06 13:01:47 +0300

    Done with switch operator

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,6 +1,29 @@
+import java.util.*;
+import java.io.*;
+
 public class BlackJack {
 	public static void main(String args[]) {
-		//test class	
-		System.out.println("I'm working in terminal bro");
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Wanna play?");
+		System.out.println("Use only \"yes\" or \"no\".");
+		
+		String res = in.nextLine();
+		
+		switch(res.toLowerCase()) {
+			case "yes":
+			System.out.println("Good!\nSo begin!");
+				break;
+			case "no":
+			System.out.println("Okay then!");
+				return;
+				
+			default:
+			System.out.println("Please use only \"yes\" or \"no\"! Thank you.");
+				return;
+		}
+
+		//Logic here
+		
 	}
 }
