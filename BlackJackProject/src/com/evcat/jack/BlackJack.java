1ed4e9d6fefaf74432786b719b24b5820ca1f95a
Evgeny Zelenin <transingularity@gmail.com>
2016-04-07 16:30:17 +0300

    Added logic for game

--- BlackJackProject/src/com/evcat/jack/BlackJack.java
+++ BlackJackProject/src/com/evcat/jack/BlackJack.java
@@ -1,6 +1,7 @@
 package com.evcat.jack;
 
 import java.util.*;
+import com.evcat.jack.RandomGenerator;
 
 public class BlackJack extends com.evcat.jack.RandomGenerator {
 	public static void main(String args[]) {
@@ -31,9 +32,38 @@ public class BlackJack extends com.evcat.jack.RandomGenerator {
 		
 		RandomGenerator bl = new RandomGenerator();
 		
-		randomInt = bl.getRandomNumbers(START, END, randNumb);
+		int sum = 0;
+		System.out.println("For quit write \"q\"\n");
+		while(in.nextLine() != "q") {
+			
+			if (sum > 21) {
+				System.out.println("Your current points: " + sum);
+				System.out.println("Yo lost!\nWanna try again?");
+				sum = 0;
+			} else if (sum == 21) {
+				System.out.println("Congrats! You win!\n");
+				sum = 0;
+			}
+			else if (sum < 21) {
+				randomInt = bl.getRandomNumbers(START, END, randNumb);
+				System.out.println("- " + randomInt + "\n");
+				sum = sum + randomInt;
+				System.out.println("Your current points: " + sum + "\n");
+				System.out.println("Want one more card?\n");
+			}
+			String needSome = in.nextLine();
+			switch (needSome.toLowerCase()) {
+				case "yes":
+					randomInt = bl.getRandomNumbers(START, END, randNumb);
+					System.out.println(randomInt + "\n");
+					sum = sum + randomInt;
+					break;
+				default:
+					System.out.println("Your final points: " + sum + "\n");
+					break;
+			}
+		}
 
-		System.out.println(randomInt);
 
 		in.close();
 		
